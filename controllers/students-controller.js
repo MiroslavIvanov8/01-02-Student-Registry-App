b434e03cdcf13441a8f836ad98744f40c1b4d138
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-10-06 19:06:14 +0300

    TitleChanged

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Devops Registry",
+      msg: "The New Devops Registry App",
       students: students
     };
     res.render('home', model);
